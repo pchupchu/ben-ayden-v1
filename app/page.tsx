daf54e69b1ce57f17cb841e8dda7772d288f5070
Marina <pchupchu@yandex.ru>
2024-09-23 20:13:36 +0300

    Добавлены шрифты, исправлены стили

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <div className='bg-site bg-no-repeat bg-cover overflow-hidden'></div>;
+  return (
+    <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
+      hello everyone!
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,36 +7,35 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    fontFamily: {
+      primary: 'var(--font-orbitron)',
+      secondary: 'var(--font-rajdhani)',
+      tertiary: 'var(--font-aldrich)',
+    },
     extend: {
-      fontFamily: {
-        primary: 'Orbitron',
-        secondary: 'Rajdhani',
-        tertiary: 'Aldrich',
+      colors: {
+        primary: '#0a0a0a',
+        accent: '#B809C3',
       },
-      container: {
-        padding: {
-          DEFAULT: '15px',
-        },
-      },
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '960px',
-        xl: '1200px',
-      },
-      extend: {
-        colors: {
-          primary: '#0a0a0a',
-          accent: '#B809C3',
-        },
-        backgroundImage: {
-          site: "url('./site-bg.jpg')",
-          about: "url('./about.png')",
-          services: "url('./services.png')",
-        },
+      backgroundImage: {
+        site: "url('./site-bg.jpg')",
+        about: "url('./about.png')",
+        services: "url('./services.png')",
       },
     },
   },
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: [],
 };
 export default config;
