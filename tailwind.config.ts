1f3d63a7e97d84496c2639643366037560e24f76
Marina <pchupchu@yandex.ru>
2024-09-23 09:40:22 +0300

    Добавлен файл prettierrc

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,42 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        primary: 'Orbitron',
+        secondary: 'Rajdhani',
+        tertiary: 'Aldrich',
+      },
+      container: {
+        padding: {
+          DEFAULT: '15px',
+        },
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '960px',
+        xl: '1200px',
+      },
+      extend: {
+        colors: {
+          primary: '#0a0a0a',
+          accent: '#B809C3',
+        },
+        backgroundImage: {
+          site: "url('./site-bg.jpg')",
+          about: "url('./about.png')",
+          services: "url('./services.png')",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require('prettier-plugin-tailwindcss')],
 };
 export default config;
